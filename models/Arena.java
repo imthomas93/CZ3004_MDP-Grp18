e1a9d0d062a03a2f37d9517ee87f7c0ad9105d95
Thomas Lim <thomaslim93@gmail.com>
2017-08-29 12:17:56 +0800

    arena and grid java updated. added mainFrameUI.java

--- models/Arena.java
+++ models/Arena.java
@@ -1,60 +1,32 @@
 package cz3004MDP.models;
 
 public class Arena {
-
-    public static final int xGRID = 15;
-    public static final int yGRID = 20;
-    public static final int START_GOAL_SIZE = 3;
-    
-    public enum state{
-    	unknown, obstacle, free
-    }
-    
-    // start zone
-    private int[] start;
-    // objective zone
-    private int[] goal;
-    // state of the specific maze's grid
-    private state[][] mazeState;
-    
-    // default constructor
-    public Arena(){}
-    
-    public Arena(int [] start, int [] goal){
-    	this.setStart(start);
-    	this.setGoal(goal);
-    	this.setMazeState(new state[yGRID][xGRID]);
-    }
-    
-    public Arena(int [] start, int [] goal, state[][] mazeState){
-    	this.setStart(start);
-    	this.setGoal(goal);
-    	this.setMazeState(mazeState);
-    }
-
-	public int[] getStart() {
-		return start;
+	
+	private static final int X_GRID = 15;
+	private static final int Y_GRID = 20;
+	private Grid state;
+	
+	// Default Constructor
+	public Arena(){
+		for (int i = 0; i < X_GRID; i++){
+			for (int j = 0; j < Y_GRID; j++){
+				state = new Grid(i,j);
+			}
+		}
 	}
-
-	public void setStart(int[] start) {
-		this.start = start;
+	
+	public void printArena(){
+		for (int i = 0; i < X_GRID; i++){
+			for (int j = 0; j < Y_GRID; j++){
+				if(!state.isExplored())
+					System.out.print("?");
+				else if(state.isClearGrid())
+					System.out.print("Y");
+				else if (state.isObstacle())
+					System.out.print("X");
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
 	}
-
-	public int[] getGoal() {
-		return goal;
-	}
-
-	public void setGoal(int[] goal) {
-		this.goal = goal;
-	}
-
-	public state[][] getMazeState() {
-		return mazeState;
-	}
-
-	public void setMazeState(state[][] mazeState) {
-		this.mazeState = mazeState;
-	}
-     
-    
 }

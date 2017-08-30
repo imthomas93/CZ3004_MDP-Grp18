d88d9f50f8461239581bc0eaab1ea115cafebfb5
Thomas Lim <thomaslim93@gmail.com>
2017-08-30 16:56:24 +0800

    fix TCP client and test socket to RPi

--- models/Sensor.java
+++ models/Sensor.java
@@ -1,5 +1,14 @@
 package cz3004MDP.models;
 
 public class Sensor {
+	private int sensorID;
+
+	public int getSensorID() {
+		return sensorID;
+	}
+
+	public void setSensorID(int sensorID) {
+		this.sensorID = sensorID;
+	}
 	
 }

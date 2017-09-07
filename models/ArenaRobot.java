a6c065c957c26e5b0d5dbf29e7aa9906e6f5730f
Thomas Lim <thomaslim93@gmail.com>
2017-09-07 23:32:06 +0800

    Robot.java setup 2

--- models/ArenaRobot.java
+++ models/ArenaRobot.java
@@ -2,10 +2,13 @@ package cz3004MDP.models;
 
 public interface ArenaRobot {
 
+	// ROBOT DIRECTION
 	static final int NORTH = 0;
 	static final int EAST = 90;
 	static final int SOUTH = 180;
 	static final int WEST = 270;
+	
+	// ARENA SIZE
 	static final int ROW = 20;
 	static final int COLUMN = 15;
 }
